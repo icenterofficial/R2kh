d7cf2ddfa2108b05b50d6721ac93c3780d38156e
iCenter <92814900+icenterofficial@users.noreply.github.com>
2026-02-13 09:09:20 +0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { WakeLockVideo } from './components/WakeLockVideo';
 
 // Updated content with static images
 const STATIC_CONTENT = [
+  "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhrC0FflqkVKUZot0LXE6Jg1G23y7qdVoHw9-59ZVTX78jEqmHYdx3OyPx9HiEgDKgcYGrFq0U_Op8nHb1G6SMede5KDkuTRnpmShUhhBnHHJKXePPwrVf43AO-6gPbZ5WD51Ivr-jQiVg2QnaQqQn1kikOR8tJG3GP4MbghyPM6deSarrMSxzOAURezt8/s1600/001.jpg",
   "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgcKgLDhK0Sdm-q7MxZSTzisBxP9fp61fzpGABZm94ilVYIfVv9Qs6Dynem2b3us6VKOZY3q3Qlf9QkVMYt-K60mtmcmEGVTCBsakksNJb5O6d2HScBXHOs0LrfTmVmUuhpAcOJwqbfcmJB_HzbaqnIEbffeVNUcJzErSZIMyBFM0seRSsb_ayAewmoUF0/s1600/man.jpg",
   "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhLXakOnv_Sj0fsN7fFHwdLgEiEsWSlIX2RBwmEfD7AES954UqbYX0hr10mgdfiiYP12CMsPa5rtbLGwyp0Xb7N8yYTQilWMmhhmb_hkIaS7N7JL1VwrCQ4SHnx1zh3ouCs-628xYPoiaeTgS76cv-Ixsp7trfaNbjQCpdmnyr2Tck6__e9SQA8XyTTIvU/s1600/women.jpg"
 ];
