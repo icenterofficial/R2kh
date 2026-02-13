197d97e0d677d04a30f453e1c298d7b08de03512
iCenter <92814900+icenterofficial@users.noreply.github.com>
2026-02-13 09:09:49 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,8 @@
-const CACHE_NAME = 'tv-slideshow-v14';
+const CACHE_NAME = 'tv-slideshow-v15';
 
 // Media List: Add the static images here so they load instantly even if offline
 const MEDIA_TO_CACHE = [
+  "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhrC0FflqkVKUZot0LXE6Jg1G23y7qdVoHw9-59ZVTX78jEqmHYdx3OyPx9HiEgDKgcYGrFq0U_Op8nHb1G6SMede5KDkuTRnpmShUhhBnHHJKXePPwrVf43AO-6gPbZ5WD51Ivr-jQiVg2QnaQqQn1kikOR8tJG3GP4MbghyPM6deSarrMSxzOAURezt8/s1600/001.jpg",
   "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgcKgLDhK0Sdm-q7MxZSTzisBxP9fp61fzpGABZm94ilVYIfVv9Qs6Dynem2b3us6VKOZY3q3Qlf9QkVMYt-K60mtmcmEGVTCBsakksNJb5O6d2HScBXHOs0LrfTmVmUuhpAcOJwqbfcmJB_HzbaqnIEbffeVNUcJzErSZIMyBFM0seRSsb_ayAewmoUF0/s1600/man.jpg",
   "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhLXakOnv_Sj0fsN7fFHwdLgEiEsWSlIX2RBwmEfD7AES954UqbYX0hr10mgdfiiYP12CMsPa5rtbLGwyp0Xb7N8yYTQilWMmhhmb_hkIaS7N7JL1VwrCQ4SHnx1zh3ouCs-628xYPoiaeTgS76cv-Ixsp7trfaNbjQCpdmnyr2Tck6__e9SQA8XyTTIvU/s1600/women.jpg"
 ];
@@ -17,7 +18,7 @@ self.addEventListener('install', (event) => {
   self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(async (cache) => {
-      console.log('Opened cache v14');
+      console.log('Opened cache v15');
       await Promise.allSettled(ASSETS_TO_CACHE.map(url => cache.add(url)));
       
       // Cache the specific images
